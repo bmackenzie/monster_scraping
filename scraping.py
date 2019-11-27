7ecfe85fdca919fd30dcb7ae50e19ff0412dd923
bmackenzie <39527613+bmackenzie@users.noreply.github.com>
2019-11-27 15:49:17 -0500

    fixing stat grabbing, creating dataframe

--- scraping.py
+++ scraping.py
@@ -2,16 +2,23 @@ import requests
 from requests import session
 from contextlib import closing
 from bs4 import BeautifulSoup
-from lxml.html import fromstring
-from itertools import cycle
+import pandas as pd
+from time import sleep
 
+#scraping url and headers
 request_url = 'https://www.dndbeyond.com/monsters/'
-
-
 
 headers = {
         'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36',
     }
+#List of monsters to be scraped
+monster_list=['aboleth', 'acolyte', 'adult-black-dragon', 'adult-blue-dragon', 'adult-brass-dragon', 'adult-bronze-dragon', 'adult-copper-dragon', 'adult-gold-dragon', 'adult-green-dragon', 'adult-red-dragon', 'adult-silver-dragon', 'adult-white-dragon', 'air-elemental', 'allosaurus', 'ancient-black-dragon', 'ancient-blue-dragon', 'ancient-brass-dragon', 'ancient-bronze-dragon', 'ancient-copper-dragon', 'ancient-gold-dragon', 'ancient-green-dragon', 'ancient-red-dragon', 'ancient-silver-dragon', 'ancient-white-dragon', 'androsphinx', 'animated-armor', 'ankheg', 'ankylosaurus', 'ape', 'archmage', 'assassin', 'awakened-shruub', 'awakened-tree', 'axe-beak', 'azer', 'baboon', 'badger', 'balor', 'bandit', 'bandit-captain']
+
+#Dataframe setup
+columns = ['name', 'size', 'type', 'alignment', 'ac', 'hp', 'move', 'str', 'dex', 'con', 'int', 'wis', 'cha', 'challenge', 'saves', 'total_save_bonus', 'skills', 'senses', 'languages', 'damage_vulnerabilities', 'damage_resistances', 'damage_immunities', 'condition_immunities']
+df= pd.DataFrame(columns=columns)
+
+
 
 def scrape(url):
     with requests.Session() as session:
@@ -31,31 +38,20 @@ def scrape(url):
         #Grabs ac, hp, movement
         for span in soup.select('.mon-stat-block__attribute-data-value'):
             stats.append(span.text.strip())
-        #Grabs saves,and total bonus
-        stats.append(soup.select('.mon-stat-block__tidbit-data')[0].text.strip())
-        total = 0
-        for x in soup.select('.mon-stat-block__tidbit-data')[0].text.strip():
-	        if x.isdigit():
-		        total += int(x)
-        stats.append(total)
-        #THIS NEEDS TO BE ADJUSTED FOR MONSTERS THAT DON'T HAVE SKILLS
-        #Grabs Skills
-        stats.append(soup.select('.mon-stat-block__tidbit-data')[1].get_text().strip())
-        #Grabs senses
-        stats.append(soup.select('.mon-stat-block__tidbit-data')[2].get_text().strip())
-        #Grabs Languages
-        stats.append(soup.select('.mon-stat-block__tidbit-data')[3].get_text().strip())
-        #Grabs CR
-        stats.append(int(soup.select('.mon-stat-block__tidbit-data')[4].get_text().strip().split(' ')[0]))
-
+        #Get ability scores
+        for span in soup.select('.ability-block__score'):
+            stats.append(float(span.text.strip()))
         #set default value to null for monsters that don't have these traits
-        #FIX THIS
-        challenge,saves,skills,senses,languages,vulnerabilities,resistences,immunities,conimmunities = ('null','null','null','null','null','null','null','null','null')
-        for i in range(len(soup.select('.mon-stat-block__tidbit-label'))-1):
-                label = soup.select('mon-stat-block__tidbit-label')[i].get_text().strip()
-                data = soup.select('mon-stat-block__tidbit-data')[i].get_text().strip()
+        challenge,saves,savetotal,skills,senses,languages,vulnerabilities,resistances,immunities,conimmunities = ('null','null','null','null','null','null','null','null','null','null')
+        for i in range(len(soup.select('.mon-stat-block__tidbit-label'))):
+                label = soup.select('.mon-stat-block__tidbit-label')[i].get_text().strip()
+                data = soup.select('.mon-stat-block__tidbit-data')[i].get_text().strip()
                 if label == 'Saving Throws':
                     saves = data
+                    savetotal = 0
+                    for x in data:
+                        if x.isdigit():
+                            savetotal += float(x)
                 elif label == 'Skills':
                     skills = data
                 elif label == 'Damage Resistances':
@@ -71,17 +67,18 @@ def scrape(url):
                 elif label == 'Languages':
                     languages = data
                 elif label == 'Challenge':
-                    challenge = data
-        stats.extend([challenge,saves,skills,senses,languages,vulnerabilities,resistences,immunities,conimmunities])
-
-        print(stats)
+                    challenge = data.split(' ')[0]
+        stats.extend([challenge,saves,savetotal,skills,senses,languages,vulnerabilities,resistances,immunities,conimmunities])
+        df.loc[len(df)] = stats
 
 
 def add_monster(monster):
     url = request_url + monster
     scrape(url)
 
-add_monster('aboleth')
+for monster in monster_list:
+    add_monster(monster)
+    sleep(20)
 
-#redo columns before making DF
-columns = ['name', 'size', 'type', 'alignment', 'ac', 'hp', 'move', 'saves', 'Totalsaves', 'challenge', 'skills', 'senses', 'languages', 'vulnerabilities', 'resistances', 'immunities', 'condition_immunities']
+print(df.head())
+df.to_csv('monster_data', index = False)
